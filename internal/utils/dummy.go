29bd56329ef86e61bf38f60f0a77f7497043aaa4
Manjunath S <ibexmonj@gmail.com>
2024-05-22 15:42:28 -0400

    team custom resource create if github has new team with requeue

---
+++ internal/utils/dummy.go
@@ -0,0 +1,27 @@
+// internal/utils/dummy.go
+package utils
+
+import (
+	"context"
+	hyperdriveharmonizeriov1 "github.com/ibexmonj/hyperdriveharmonizer/api/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func CreateDummyTeam(ctx context.Context, client client.Client) error {
+	// Create a "dummy" Team resource
+	dummyTeam := &hyperdriveharmonizeriov1.Team{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "dummy",
+			Namespace: "default", // Modify as needed
+		},
+		Spec: hyperdriveharmonizeriov1.TeamSpec{
+			TeamName: "dummy",
+			Members:  []string{},
+		},
+	}
+	if err := client.Create(ctx, dummyTeam); err != nil {
+		return err
+	}
+	return nil
+}
