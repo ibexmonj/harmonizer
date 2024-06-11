9e9e6394ba8e32c3610d22bb3e0054f1328e1e22
Manjunath S <ibexmonj@gmail.com>
2024-06-10 21:52:04 -0400

    github client interface and adjust createnamespace logic so its re-created on reconciliation if deleted. gs

--- internal/controller/github.go
+++ internal/controller/github.go
@@ -5,33 +5,22 @@ import (
 	"fmt"
 	"github.com/google/go-github/v33/github"
 	harmonizeriov1 "github.com/ibexmonj/harmonizer/api/v1beta1"
-	"golang.org/x/oauth2"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"os"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-func FetchAndCreateTeams(ctx context.Context, r *TeamReconciler, req ctrl.Request) error {
+type GitHubClient interface {
+	ListTeams(ctx context.Context, org string, opt *github.ListOptions) ([]*github.Team, *github.Response, error)
+	ListTeamMembersBySlug(ctx context.Context, org string, slug string, opt *github.TeamListTeamMembersOptions) ([]*github.User, *github.Response, error)
+}
+
+func FetchAndCreateTeams(ctx context.Context, r *TeamReconciler, req ctrl.Request, ghClient GitHubClient) error {
 	log := log.FromContext(ctx)
 
-	// Set up the GitHub client
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-
-	ghClient := github.NewClient(tc)
-	// Get the list of teams from GitHub
-	orgs, _, err := ghClient.Organizations.List(ctx, "", nil)
-	if err != nil {
-		log.Error(err, "failed to get Org from GitHub")
-		return err
-	}
-	fmt.Println(orgs, ": organization found")
-	teams, _, err := ghClient.Teams.ListTeams(ctx, "PistonPioneers", nil)
+	teams, _, err := ghClient.ListTeams(ctx, "PistonPioneers", nil)
 	if err != nil {
 		log.Error(err, "failed to get teams from GitHub")
 		return err
@@ -46,13 +35,13 @@ func FetchAndCreateTeams(ctx context.Context, r *TeamReconciler, req ctrl.Reques
 		// Check if the custom resource already exists
 		namespacedName := types.NamespacedName{
 			Name:      teamName,
-			Namespace: "default", // TODO: Modify as needed
+			Namespace: "default", // TODO: revisit
 		}
 		var existingTeam harmonizeriov1.Team
 		if err := r.Get(ctx, namespacedName, &existingTeam); err != nil {
 			if errors.IsNotFound(err) {
 
-				members, _, err := ghClient.Teams.ListTeamMembersBySlug(ctx, "PistonPioneers", *team.Slug, nil)
+				members, _, err := ghClient.ListTeamMembersBySlug(ctx, "PistonPioneers", *team.Slug, nil)
 				if err != nil {
 					log.Error(err, "failed to get team members from GitHub")
 					return err
@@ -71,7 +60,7 @@ func FetchAndCreateTeams(ctx context.Context, r *TeamReconciler, req ctrl.Reques
 				newTeam := &harmonizeriov1.Team{
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      teamName,
-						Namespace: "default", // TODO: Modify as needed
+						Namespace: "default", // TODO: revisit
 					},
 					Spec: harmonizeriov1.TeamSpec{
 						TeamName: teamName,
@@ -84,16 +73,16 @@ func FetchAndCreateTeams(ctx context.Context, r *TeamReconciler, req ctrl.Reques
 				}
 				log.Info("created Team resource", "name", teamName)
 
-				// Create a namespace for the team
-				if err := CreateNamespace(ctx, r, teamName); err != nil {
-					return err
-				}
-
 			} else {
 				// Error reading the object - requeue the request.
 				log.Error(err, "failed to get Team resource")
 				return err
 			}
+		}
+
+		// Create a namespace for the team
+		if err := CreateNamespace(ctx, r, teamName); err != nil {
+			return err
 		}
 	}
 
