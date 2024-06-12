2612d17b05a5003031e710ffced624fd0ae6985a
Manjunath S <ibexmonj@gmail.com>
2024-06-11 20:36:59 -0400

    refactor fetchand create, cleaner code

--- internal/controller/github.go
+++ internal/controller/github.go
@@ -9,7 +9,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 type GitHubClient interface {
@@ -17,71 +16,68 @@ type GitHubClient interface {
 	ListTeamMembersBySlug(ctx context.Context, org string, slug string, opt *github.TeamListTeamMembersOptions) ([]*github.User, *github.Response, error)
 }
 
-func FetchAndCreateTeams(ctx context.Context, r *TeamReconciler, req ctrl.Request, ghClient GitHubClient) error {
-	log := log.FromContext(ctx)
-
+// FetchTeams fetches teams from GitHub.
+func FetchTeams(ctx context.Context, ghClient GitHubClient) ([]*github.Team, error) {
 	teams, _, err := ghClient.ListTeams(ctx, "PistonPioneers", nil)
 	if err != nil {
-		log.Error(err, "failed to get teams from GitHub")
+		return nil, fmt.Errorf("failed to get teams from GitHub: %w", err)
+	}
+	return teams, nil
+}
+
+// CreateTeamResource creates a new Team resource in Kubernetes if it doesn't already exist.
+func CreateTeamResource(ctx context.Context, r *TeamReconciler, team *github.Team, ghClient GitHubClient) error {
+	teamName := team.GetName()
+	namespacedName := types.NamespacedName{
+		Name:      teamName,
+		Namespace: "default", // TODO: revisit
+	}
+	var existingTeam harmonizeriov1.Team
+	if err := r.Get(ctx, namespacedName, &existingTeam); err != nil {
+		if errors.IsNotFound(err) {
+			members, _, err := ghClient.ListTeamMembersBySlug(ctx, "PistonPioneers", *team.Slug, nil)
+			if err != nil {
+				return fmt.Errorf("failed to get team members from GitHub: %w", err)
+			}
+			memberNames := make([]string, 0, len(members))
+			for _, member := range members {
+				if member.Login != nil {
+					memberNames = append(memberNames, *member.Login)
+				}
+			}
+			newTeam := &harmonizeriov1.Team{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      teamName,
+					Namespace: "default", // TODO: revisit
+				},
+				Spec: harmonizeriov1.TeamSpec{
+					TeamName: teamName,
+					Members:  memberNames,
+				},
+			}
+			if err := r.Create(ctx, newTeam); err != nil {
+				return fmt.Errorf("failed to create Team resource: %w", err)
+			}
+		} else {
+			return fmt.Errorf("failed to get Team resource: %w", err)
+		}
+	}
+	return nil
+}
+
+// FetchAndCreateTeams fetches teams from GitHub and creates corresponding resources in Kubernetes.
+func FetchAndCreateTeams(ctx context.Context, r *TeamReconciler, req ctrl.Request, ghClient GitHubClient) error {
+	teams, err := FetchTeams(ctx, ghClient)
+	if err != nil {
 		return err
 	}
 
-	fmt.Println("teams found:", teams)
-
-	// For each team, ensure there's a corresponding Kubernetes custom resource
 	for _, team := range teams {
-		teamName := team.GetName()
-
-		// Check if the custom resource already exists
-		namespacedName := types.NamespacedName{
-			Name:      teamName,
-			Namespace: "default", // TODO: revisit
-		}
-		var existingTeam harmonizeriov1.Team
-		if err := r.Get(ctx, namespacedName, &existingTeam); err != nil {
-			if errors.IsNotFound(err) {
-
-				members, _, err := ghClient.ListTeamMembersBySlug(ctx, "PistonPioneers", *team.Slug, nil)
-				if err != nil {
-					log.Error(err, "failed to get team members from GitHub")
-					return err
-				}
-				fmt.Println("members found:", members)
-
-				// Convert the members to a slice of strings
-				memberNames := make([]string, 0, len(members))
-				for _, member := range members {
-					if member.Login != nil {
-						memberNames = append(memberNames, *member.Login)
-					}
-				}
-
-				// The custom resource doesn't exist, so create it
-				newTeam := &harmonizeriov1.Team{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      teamName,
-						Namespace: "default", // TODO: revisit
-					},
-					Spec: harmonizeriov1.TeamSpec{
-						TeamName: teamName,
-						Members:  memberNames,
-					},
-				}
-				if err := r.Create(ctx, newTeam); err != nil {
-					log.Error(err, "failed to create Team resource")
-					return err
-				}
-				log.Info("created Team resource", "name", teamName)
-
-			} else {
-				// Error reading the object - requeue the request.
-				log.Error(err, "failed to get Team resource")
-				return err
-			}
+		if err := CreateTeamResource(ctx, r, team, ghClient); err != nil {
+			return err
 		}
 
-		// Create a namespace for the team
-		if err := CreateNamespace(ctx, r, teamName); err != nil {
+		if err := CreateNamespace(ctx, r, team.GetName()); err != nil {
 			return err
 		}
 	}
